3fe8eec88b040eb9285a928684bc736b64f27dac
Nabari4 <nabarovs@mail.ru>
2019-04-01 10:21:19 +0300

    Создание класса

---
+++ Практика 1/Практика 1/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика_1
+{
+    class Student
+    {
+        public string Name { get; set; } // Имя
+        public int Curs { get; set; } //Курс
+        public string Pol { get; set; } //Пол
+        public Student()
+        {
+            Name = null;
+            Curs = 0;
+            Pol = null;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Имя: {0} Курс: {1} Пол: {2}", Name, Curs, Pol);
+        }
+        public void SetInfo()
+        {
+            Console.WriteLine("Введите имя студента: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите курс студента: ");
+            Curs = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите пол студента: ");
+            Pol = Console.ReadLine();
+        }
+    }
+}
